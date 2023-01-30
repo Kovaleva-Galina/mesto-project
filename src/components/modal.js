6af870557ce32b57de6c92edd074a1368c89e440
Galina Kovaleva <galkina.galina.alexandrovna@yandex.ru>
2023-01-30 20:48:25 +0300

    fix: исправлены ошибки после ревью 2

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,9 +1,26 @@
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const nameInput = popupEditProfile.querySelector('.popup__input_type_name');
+const jobInput = popupEditProfile.querySelector('.popup__input_type_job');
+const profileVocation = document.querySelector('.profile__vocation');
+const profileName = document.querySelector('.profile__name');
+
+export function closePopupByEsc(evt) {
+  if (evt.key === 'Escape') {
+    const popupOpen = document.querySelector('.popup_opened');
+    if (popupOpen !== null) {
+      closePopup(popupOpen);
+    }
+  };
+}
+
 export function openPopup(popupType) {
   popupType.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupByEsc);
 }
 
 export function closePopup(popupType) {
   popupType.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupByEsc);
 }
 
 export const closePopupsByClickOnSection = () => {
@@ -17,34 +34,13 @@ export const closePopupsByClickOnSection = () => {
   })
 }
 
-export const closePopupByEsc = () => {
-  document.addEventListener('keydown', function(evt) {
-    if (evt.key === 'Escape') {
-      const popupOpen = document.querySelector('.popup_opened');
-      if (popupOpen !== null) {
-        closePopup(popupOpen);
-      }
-    };
-  });
-}
-
 export function initEditProfile() {
-  const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-  const nameInput = popupEditProfile.querySelector('.popup__input_type_name');
-  const jobInput = popupEditProfile.querySelector('.popup__input_type_job');
-  const profileVocation = document.querySelector('.profile__vocation');
-  const profileName = document.querySelector('.profile__name');
   nameInput.value = profileName.textContent;
   jobInput.value = profileVocation.textContent;
 }
 
 export function handleEditProfile(evt) {
   evt.preventDefault();
-  const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-  const nameInput = popupEditProfile.querySelector('.popup__input_type_name');
-  const jobInput = popupEditProfile.querySelector('.popup__input_type_job');
-  const profileVocation = document.querySelector('.profile__vocation');
-  const profileName = document.querySelector('.profile__name');
   profileVocation.textContent = jobInput.value;
   profileName.textContent = nameInput.value;
   closePopup(popupEditProfile);
